cf4a85b433b141fe6103afb76119b4f40f12d23e
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-12-05 15:17:10 +0530

    Add readFileData function

--- src/lib.js
+++ src/lib.js
@@ -7,18 +7,15 @@ const classifyDetails = function(details) {
   };
 };
 
-const extractLines = function(length, contents) {
-  return contents.split("\n").splice(0,length).join("\n");
-};
-
-const extractCharacters = function(length, contents) {
-  return contents.split("").splice(0,length).join("");
+const getFileData = function(data, type='n', length=10) {
+  if(type == 'n') {
+    return data.split('\n').slice(0,length).join('\n');
+  }
+  return data.split('').slice(0,length).join('');
 };
 
 const readFile = function(readFileSync, files) { 
   return files.map((file) => readFileSync(file,'utf8'));
 };
 
-module.exports = {
-  classifyDetails, extractLines, 
-  extractCharacters, readFile };
+module.exports = { classifyDetails, readFile };
--- test/libTest.js
+++ test/libTest.js
@@ -7,7 +7,7 @@ const {
 
 let returnConstant = function(constant){ return constant; }; 
 
-describe('classifyDetails categories the input according to characteristics', () => {
+describe('classifyDetails', () => {
   it('should return empty object for no input', () => {
     deepEqual(classifyDetails(),{});
   });
@@ -23,45 +23,7 @@ describe('classifyDetails categories the input according to characteristics', ()
   });
 });
 
-describe('extractLines returns lines of given text as per the given input', () => {
-  it('should return empty string for 0 length input', () => {
-    deepEqual(extractLines(0,'first line\nsecond line'),'');
-  });
-
-  it('should return one line for length as input 1', () => {
-    deepEqual(extractLines(1,'first line\nsecond line'),'first line');
-  });
-
-  it('should return empty line for invalid length(negative)', () => {
-    deepEqual(extractLines(-1,'first line\nsecond line'),'');
-  });
-
-  it('should return number of lines as per the given length', () => {
-    deepEqual(extractLines(2,'first line\nsecond line'),'first line\nsecond line');
-    deepEqual(extractLines(4,'first\nline\nsecond\nline'),'first\nline\nsecond\nline');
-  });
-});
-
-describe('extractCharacters returns characters of given text as per the given input length', () => {
-  it('should return empty string for 0 length input', () => {
-    deepEqual(extractCharacters(0,'first line\nsecond line'),'');
-  });
-
-  it('should return one character for length as input 1', () => {
-    deepEqual(extractCharacters(1,'first line\nsecond line'),'f');
-  });
-
-  it('should return empty character for invalid length(negative)', () => {
-    deepEqual(extractCharacters(-1,'first line\nsecond line'),'');
-  });
-
-  it('should return number of characters as per the given length', () => {
-    deepEqual(extractCharacters(2,'first line\nsecond line'),'fi');
-    deepEqual(extractCharacters(5,'first\nline\nsecond\nline'),'first');
-  });
-});
-
-describe('readFile returns the result as per the mapper function', () => {
+describe('readFile', () => {
   it('should return same output as per the input', () => {
     deepEqual(readFile(returnConstant,[0,0,0]),[0,0,0]);
     deepEqual(readFile(returnConstant,['a','a','a']),['a','a','a']);
